743077529df879cd58d77f5e22b822878cca90a3
Artem Antonenko <a-.-@MacBook-Pro-Artem.local>
2023-01-27 19:07:40 +0300

    5

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numA = 3;
-int numB = 5;
+﻿int numA = new Random().Next(1, 10); // 1...9
+Console.WriteLine(numA);
+
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
+
 int sum = (numA + numB);
 Console.WriteLine(sum);
